fc7e7bc1ad888486086d13ae208d52ef28142d03
Penosina <penosina@mail.ru>
2022-04-20 21:26:06 +0700

    Initial commit

---
+++ WS_Qualifying/WS_Qualifying/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WS_Qualifying
+//
+//  Created by Илья Абросимов on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
